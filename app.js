a5be61eb2a8ea2b9bb7f8e2da6665bd3a4563465
Hung Y.F <hugh.12345zxcvb@gmail.com>
2019-09-08 18:38:13 +0800

    deploy(success) deploy to heroku

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const app = express();
 const logger = require('morgan');
+const path = require('path');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./app_server/routes/index');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
   res.json({ data: 'hello world' });
-});
+});*/
 app.use('/', indexRouter);
+
+//link index.html
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/public/index.html', function(err) {
+    if (err) res.send(404);
+  });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, '/public')));
+app.use('/js', express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/public')));
 
 app.use(function(err, req, res, next) {
   if (err.code >= 500) {
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const testRouter = require('./test');
+const testRouter = require('./tests');
 const userRouter = require('./users');
 
 router.use('/users', userRouter);
